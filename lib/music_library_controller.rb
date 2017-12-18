5b0f68bff5b3ac3435c6cbaae9e6ca9761c92233
Araba Taylor <araba.taylor20@gmail.com>
2017-12-18 21:02:10 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,8 +71,8 @@ class MusicLibraryController
     #select song from song list
     #puts playing 'song name' by 'song artist'
     puts "Which song number would you like to play?"
-    user_input = gets.strip
     binding.pry
+    user_input = gets.strip
     Sorted_list = Song.all.uniq.sort{ |a, b| a.name <=> b.name }
   end
 

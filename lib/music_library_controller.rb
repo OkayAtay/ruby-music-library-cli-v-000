bf5a8bfb0285a0db4865bc49992a1a35b31f4014
Araba Taylor <araba.taylor20@gmail.com>
2017-12-18 21:52:44 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,6 +20,22 @@ class MusicLibraryController
       puts("What would you like to do?")
 
       user_input = gets.chomp
+
+
+      case user_input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
